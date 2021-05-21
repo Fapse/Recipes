18054ac742e31c5ab3a7bd48180e774e82891a76
Fapse <fb-online@posteo.de>
2021-05-21 22:10:57 +0200

    RecipeEdit adapted for recipe properties instructions and ingredients.

--- Recipes/Views/RecipeEdit.swift
+++ Recipes/Views/RecipeEdit.swift
@@ -14,22 +14,39 @@ struct RecipeEdit: View {
 	var recipe: Recipe
 	
 	@State private var name: String = ""
+	@State private var ingredients: String = ""
+	@State private var instructions: String = ""
 	
 	init(recipe: Recipe) {
 		self.recipe = recipe
 		
 		_name = State(initialValue: recipe.name)
+		_ingredients = State(initialValue: recipe.ingredients)
+		_instructions = State(initialValue: recipe.instructions)
 	}
 
     var body: some View {
         NavigationView {
 			Form {
-				TextField("Name", text: $name)
+				Section(header: Text("Rezeptname")) {
+					TextField("Name", text: $name)
+				}
+				Section(header: Text("Zutaten")) {
+					TextEditor(text: $ingredients)
+						.frame(height: 150)
+					
+				}
+				Section(header: Text("Kochanleitung")) {
+					TextEditor(text: $instructions)
+						.frame(height: 150)
+				}
 			}
-			.navigationBarTitle(Text("Rezept ändern"), displayMode: .inline)
+			.navigationBarTitle(Text("Rezept bearbeiten"), displayMode: .inline)
 			.navigationBarItems(trailing:
-				Button("Save") {
+				Button("Speichern") {
 					recipe.name = name
+					recipe.ingredients = ingredients
+					recipe.instructions = instructions
 					try? managedObjectContext.save()
 					presentationMode.wrappedValue.dismiss()
 				}
