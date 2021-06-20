5357464dbdf87b2843695b09cae0fa9b489d9a62
Fapse <fb-online@posteo.de>
2021-06-20 11:04:30 +0200

    Added JSON Export to Settings.view

--- Recipes/Views/Settings.swift
+++ Recipes/Views/Settings.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct Settings: View {
+
+	@Environment(\.managedObjectContext) var managedObjectContext
+	
+	@FetchRequest(
+		entity: Recipe.entity(),
+		sortDescriptors: [NSSortDescriptor(keyPath: \Recipe.name, ascending: true)]
+	) var recipes: FetchedResults<Recipe>
+
+
     var body: some View {
 		VStack {
 		Group {
@@ -30,51 +39,39 @@ struct Settings: View {
 	}
 
 	func exportRecipesToFile() {
-		writeToFile()
+
+		if recipes.count > 0 {
+			var json = "{\n"
+			for i in 0...recipes.count - 1 {
+				json.append("\t[\n")
+				json.append("\t\t\"name\": \"\(recipes[i].name)\",\n")
+				json.append("\t\t\"ingredients\": \"\(recipes[i].ingredients)\",\n")
+				json.append("\t\t\"instructions\": \"\(recipes[i].instructions)\"\n")
+				json.append("\t]")
+				if !(i == recipes.count - 1) {
+					json.append(",\n")
+				}
+			}
+			json.append("\n}")
+			print(json)
+			writeToFile(json)
+		}
 	}
     
     func deleteRecipeDatabase(){
     
 	}
-
-
-
-    func saveFile() {
-		let str = "My string"
-		let filename = getDocumentsDirectory()?.appendingPathComponent("output.txt")
-		do {
-			try str.write(to: filename!, atomically: true, encoding: String.Encoding.utf8)
-		} catch {
-			print("Problem saving file")
-		// failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
-		}
-	}
 	
-	func writeToFile() {
-		let todos = "Eat;Sleep;Repeat;TV"
+	func writeToFile(_ fileContent: String) {
 		let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask) [0].appendingPathComponent("recipes.txt")
 		do {
 			print(path.absoluteString)
 			print("... attempting to write")
-			try todos.write(to: path, atomically: true, encoding: .utf8)
+			try fileContent.write(to: path, atomically: true, encoding: .utf8)
 			print("... file writtem")
 		} catch {
 			print(error.localizedDescription)
 		}
-	}
-    
-    func getDocumentsDirectory() -> URL? {
-		let paths = FileManager.default.urls(for: .allLibrariesDirectory, in: .userDomainMask)
-		print(paths.count)
-		var url: URL?
-		if (paths.count > 0) {
-			for path in paths {
-				print (path)
-			}
-			url = paths[0]
-		}
-		//print(paths[0])
-		return url
 	}
 }
 
